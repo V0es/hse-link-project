7e7c3e6c8d7bb5668d3684600218c4b316bb7ba3
Max Elnikov <elnikow.max@gmail.com>
2026-03-12 23:15:20 +0300

    added user schemas

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
 
 
-class UserCreate(BaseModel):
+class UserSchema(BaseModel):
     username: str
     password_hash: str
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str = "Bearer"
