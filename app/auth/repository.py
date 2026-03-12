98cf693f14ac5f305b3556dee9aabb9a91e85d1f
Max Elnikov <elnikow.max@gmail.com>
2026-03-12 23:14:27 +0300

    added sql user repo

--- app/auth/repository.py
+++ app/auth/repository.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth.base_repo import AbstractUserRepository
 from app.auth.models import User
-from app.auth.schemas import UserCreate
+from app.auth.schemas import UserSchema
 
 
 class SQLUserRepository(AbstractUserRepository):
@@ -20,7 +20,7 @@ class SQLUserRepository(AbstractUserRepository):
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def create(self, user_schema: UserCreate) -> User:
+    async def create(self, user_schema: UserSchema) -> User:
         user_to_create = User(**user_schema.model_dump())
         self.session.add(user_to_create)
 
