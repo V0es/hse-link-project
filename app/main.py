dce06e3f9f670cba175e331fa2afe7becf3570f5
Max Elnikov <elnikow.max@gmail.com>
2026-03-15 01:23:21 +0300

    added redis to app state

--- app/main.py
+++ app/main.py
@@ -1,15 +1,20 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-
 from core.config import get_settings
 from core.database import engine
+from fastapi import FastAPI
+
+from app.core.redis import get_redis
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    app.state.redis = await get_redis(
+        host=settings.cache.host, port=settings.cache.port, db_num=settings.cache.db_num
+    )
     yield
     await engine.dispose()
+    await app.state.redis.close()
 
 
 settings = get_settings()
