c06c316ccfa0922faaf7fcb9ea7546f86aa6410c
Max Elnikov <elnikow.max@gmail.com>
2026-03-15 01:30:04 +0300

    implemented creating and redirecting endpoints

--- app/links/router.py
+++ app/links/router.py
@@ -1,22 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import RedirectResponse
+
+from app.auth.deps import get_current_user
+from app.auth.models import User
+from app.links.deps import get_link_service
+from app.links.schemas import LinkCreate
+from app.links.service import LinkServive
 
 router = APIRouter(prefix="/links")
 
 
-@router.post("/shorten")
-def create_short_link():
+@router.post("/shorten", response_model=LinkCreate, status_code=status.HTTP_201_CREATED)
+async def create_short_link(
+    link: LinkCreate,
+    link_service: LinkServive = Depends(get_link_service),
+    user: User | None = Depends(get_current_user),
+) -> LinkCreate:
     """
     Create short link
     """
-    pass
+    created_link = await link_service.create_short_link(link, user)
+    return created_link
 
 
 @router.get("/{short_code}")
-def redirect_to_original_link(short_code: str):
+async def redirect_to_original_link(
+    short_code: str, link_service: LinkServive = Depends(get_link_service)
+) -> RedirectResponse:
     """
     Redirect
     """
-    pass
+    original_url = await link_service.get_original_link(short_code)
+    return RedirectResponse(original_url)
 
 
 @router.delete("/{short_code}")
